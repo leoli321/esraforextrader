74c321d926caa120ce1aebb7f2033306af11e746
asekharan7 <asekharan7@gatech.edu>
2021-04-13 15:37:15 -0400

    first commit

---
+++ trading_ig_config.py
@@ -0,0 +1,6 @@
+class config(object):
+    username = "esrainvestments"
+    password = "GetBread@Esra1"
+    api_key = "YOUR_API_KEY"
+    acc_type = "DEMO" # LIVE / DEMO
+    acc_number = "ABC123"
